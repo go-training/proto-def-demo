8494f4da8cc6070c4393d631f4406cf69d4d03bf
Bo-Yi Wu <appleboy.tw@gmail.com>
2022-08-13 19:57:58 +0800

    chore(gin): Add ping service
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

---
+++ cmd/server/gin/gitea.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	v1 "github.com/go-training/proto-connect-demo/gen/proto/v1"
+	"github.com/go-training/proto-connect-demo/gen/proto/v1/v1connect"
+
+	"github.com/bufbuild/connect-go"
+	grpchealth "github.com/bufbuild/connect-grpchealth-go"
+	grpcreflect "github.com/bufbuild/connect-grpcreflect-go"
+)
+
+type GiteaServer struct{}
+
+func (s *GiteaServer) Gitea(
+	ctx context.Context,
+	req *connect.Request[v1.GiteaRequest],
+) (*connect.Response[v1.GiteaResponse], error) {
+	log.Println("Content-Type: ", req.Header().Get("Content-Type"))
+	log.Println("User-Agent: ", req.Header().Get("User-Agent"))
+	res := connect.NewResponse(&v1.GiteaResponse{
+		Giteaing: fmt.Sprintf("Hello, %s!", req.Msg.Name),
+	})
+	res.Header().Set("Gitea-Version", "v1")
+	return res, nil
+}
+
+func giteaServiceRoute(r *gin.Engine) {
+	compress1KB := connect.WithCompressMinBytes(1024)
+
+	giteaService := &GiteaServer{}
+	connectPath, connecthandler := v1connect.NewGiteaServiceHandler(
+		giteaService,
+		compress1KB,
+	)
+
+	// grpcV1
+	grpcPath, gHandler := grpcreflect.NewHandlerV1(
+		grpcreflect.NewStaticReflector(v1connect.GiteaServiceName),
+		compress1KB,
+	)
+
+	// grpcV1Alpha
+	grpcAlphaPath, gAlphaHandler := grpcreflect.NewHandlerV1Alpha(
+		grpcreflect.NewStaticReflector(v1connect.GiteaServiceName),
+		compress1KB,
+	)
+
+	// grpcHealthCheck
+	grpcHealthPath, gHealthHandler := grpchealth.NewHandler(
+		grpchealth.NewStaticChecker(v1connect.GiteaServiceName),
+		compress1KB,
+	)
+
+	r.POST(connectPath+":name", grpcHandler(connecthandler))
+	r.POST(grpcPath+"Gitea", grpcHandler(gHandler))
+	r.POST(grpcAlphaPath+"Gitea", grpcHandler(gAlphaHandler))
+	r.POST(grpcHealthPath+"Gitea", grpcHandler(gHealthHandler))
+}
---
+++ cmd/server/gin/ping.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	v1 "github.com/go-training/proto-connect-demo/gen/proto/v1"
+	"github.com/go-training/proto-connect-demo/gen/proto/v1/v1connect"
+
+	"github.com/bufbuild/connect-go"
+	grpchealth "github.com/bufbuild/connect-grpchealth-go"
+	grpcreflect "github.com/bufbuild/connect-grpcreflect-go"
+)
+
+type PingService struct{}
+
+func (s *PingService) Ping(
+	ctx context.Context,
+	req *connect.Request[v1.PingRequest],
+) (*connect.Response[v1.PingResponse], error) {
+	log.Println("Content-Type: ", req.Header().Get("Content-Type"))
+	log.Println("User-Agent: ", req.Header().Get("User-Agent"))
+	res := connect.NewResponse(&v1.PingResponse{
+		Data: fmt.Sprintf("Hello, %s!", req.Msg.Data),
+	})
+	res.Header().Set("Gitea-Version", "v1")
+	return res, nil
+}
+
+func pingServiceRoute(r *gin.Engine) {
+	compress1KB := connect.WithCompressMinBytes(1024)
+
+	pingService := &PingService{}
+	connectPath, connecthandler := v1connect.NewPingServiceHandler(
+		pingService,
+		compress1KB,
+	)
+
+	// grpcV1
+	grpcPath, gHandler := grpcreflect.NewHandlerV1(
+		grpcreflect.NewStaticReflector(v1connect.PingServiceName),
+		compress1KB,
+	)
+
+	// grpcV1Alpha
+	grpcAlphaPath, gAlphaHandler := grpcreflect.NewHandlerV1Alpha(
+		grpcreflect.NewStaticReflector(v1connect.PingServiceName),
+		compress1KB,
+	)
+
+	// grpcHealthCheck
+	grpcHealthPath, gHealthHandler := grpchealth.NewHandler(
+		grpchealth.NewStaticChecker(v1connect.PingServiceName),
+		compress1KB,
+	)
+
+	r.POST(connectPath+":name", grpcHandler(connecthandler))
+	r.POST(grpcPath+"Ping", grpcHandler(gHandler))
+	r.POST(grpcAlphaPath+"Ping", grpcHandler(gAlphaHandler))
+	r.POST(grpcHealthPath+"Ping", grpcHandler(gHealthHandler))
+}
